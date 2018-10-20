9197120ad71c6cc20fd20721215fbd6cbd7b355c
Rui Pires <rui@sennin.pt>
2018-10-20 12:01:08 +0000

    improves _run.sh script

--- _run.sh
+++ _run.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
+
 #sudo gem uninstall jasper2
-sudo gem install _gems/jasper2-2.1.9.gem
-bundle exec jekyll liveserve --host 0.0.0.0 --watch --force-polling
+
+if [ "$1" == "gem" ]
+then
+    echo "Installing gem ..."
+    sudo gem install _gems/jasper2-2.1.9.gem
+fi
+
+bundle exec jekyll liveserve --host 0.0.0.0 --watch
 #bundle exec jekyll liveserve --host 0.0.0.0 --incremental --watch
 #bundle exec jekyll serve --host 0.0.0.0 --incremental --watch --force_polling
 #bundle exec jekyll serve --host 0.0.0.0 
